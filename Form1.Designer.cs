549759de1af42444df00a340973ff2450854c680
tjdarr79 <tjdarr79@gmail.com>
2017-03-07 09:22:59 -0800

    added MS_live button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,14 +33,14 @@
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.btn_Submit = new System.Windows.Forms.Button();
             this.txb_userName = new System.Windows.Forms.TextBox();
-            this.txb_password = new System.Windows.Forms.TextBox();
-            this.txb_confirmPass = new System.Windows.Forms.TextBox();
+            this.btn_MS_Live = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // txb_firstName
             // 
             this.txb_firstName.Font = new System.Drawing.Font("Trebuchet MS", 8.25F);
-            this.txb_firstName.Location = new System.Drawing.Point(22, 278);
+            this.txb_firstName.Location = new System.Drawing.Point(12, 13);
             this.txb_firstName.Name = "txb_firstName";
             this.txb_firstName.Size = new System.Drawing.Size(100, 20);
             this.txb_firstName.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // txb_lastName
             // 
-            this.txb_lastName.Location = new System.Drawing.Point(149, 278);
+            this.txb_lastName.Location = new System.Drawing.Point(139, 13);
             this.txb_lastName.Name = "txb_lastName";
             this.txb_lastName.Size = new System.Drawing.Size(100, 20);
             this.txb_lastName.TabIndex = 1;
@@ -56,55 +56,58 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(22, 12);
+            this.webBrowser1.Location = new System.Drawing.Point(12, 117);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1090, 250);
+            this.webBrowser1.Size = new System.Drawing.Size(556, 334);
             this.webBrowser1.TabIndex = 2;
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
             // btn_Submit
             // 
-            this.btn_Submit.Location = new System.Drawing.Point(439, 427);
+            this.btn_Submit.Location = new System.Drawing.Point(78, 65);
             this.btn_Submit.Name = "btn_Submit";
             this.btn_Submit.Size = new System.Drawing.Size(75, 23);
             this.btn_Submit.TabIndex = 3;
-            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.Text = "Gmail";
             this.btn_Submit.UseVisualStyleBackColor = true;
             this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
             // 
             // txb_userName
             // 
-            this.txb_userName.Location = new System.Drawing.Point(22, 304);
+            this.txb_userName.Location = new System.Drawing.Point(12, 39);
             this.txb_userName.Name = "txb_userName";
             this.txb_userName.Size = new System.Drawing.Size(227, 20);
             this.txb_userName.TabIndex = 4;
             this.txb_userName.Text = "@Gmail.com";
             this.txb_userName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // txb_password
+            // btn_MS_Live
             // 
-            this.txb_password.Location = new System.Drawing.Point(22, 330);
-            this.txb_password.Name = "txb_password";
-            this.txb_password.Size = new System.Drawing.Size(227, 20);
-            this.txb_password.TabIndex = 5;
-            this.txb_password.Text = "Password";
+            this.btn_MS_Live.Location = new System.Drawing.Point(695, 64);
+            this.btn_MS_Live.Name = "btn_MS_Live";
+            this.btn_MS_Live.Size = new System.Drawing.Size(75, 23);
+            this.btn_MS_Live.TabIndex = 5;
+            this.btn_MS_Live.Text = "MS Live";
+            this.btn_MS_Live.UseVisualStyleBackColor = true;
+            this.btn_MS_Live.Click += new System.EventHandler(this.btn_MS_Live_Click);
             // 
-            // txb_confirmPass
+            // webBrowser2
             // 
-            this.txb_confirmPass.Location = new System.Drawing.Point(22, 357);
-            this.txb_confirmPass.Name = "txb_confirmPass";
-            this.txb_confirmPass.Size = new System.Drawing.Size(227, 20);
-            this.txb_confirmPass.TabIndex = 6;
-            this.txb_confirmPass.Text = "Confirm Password";
+            this.webBrowser2.Location = new System.Drawing.Point(574, 117);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(549, 334);
+            this.webBrowser2.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1124, 479);
-            this.Controls.Add(this.txb_confirmPass);
-            this.Controls.Add(this.txb_password);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.btn_MS_Live);
             this.Controls.Add(this.txb_userName);
             this.Controls.Add(this.btn_Submit);
             this.Controls.Add(this.webBrowser1);
@@ -124,8 +127,8 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Button btn_Submit;
         private System.Windows.Forms.TextBox txb_userName;
-        private System.Windows.Forms.TextBox txb_password;
-        private System.Windows.Forms.TextBox txb_confirmPass;
+        private System.Windows.Forms.Button btn_MS_Live;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,8 +24,11 @@ namespace Gmail_MS_builder
             string firstName = txb_firstName.Text;
             string lastName = txb_lastName.Text;
             string userName = txb_userName.Text;
-            string password = txb_password.Text;
-            string confirmPass = txb_confirmPass.Text;
+            string password = "1345698746";
+            string confirmPass = "1345698746";
+            string birthDay = "15";
+            string birthYear = "1985";
+
 
             //fill ion web form by ID
             webBrowser1.Document.GetElementById("FirstName").SetAttribute("value", firstName);
@@ -33,7 +36,9 @@ namespace Gmail_MS_builder
             webBrowser1.Document.GetElementById("GmailAddress").SetAttribute("value", userName);
             webBrowser1.Document.GetElementById("Passwd").SetAttribute("value", password);
             webBrowser1.Document.GetElementById("PasswdAgain").SetAttribute("value", confirmPass);
-
+            //webBrowser1.Document.GetElementById("BirthMonth").SetAttribute("value", "Febuary");
+            webBrowser1.Document.GetElementById("BirthDay").SetAttribute("value", birthDay);
+            webBrowser1.Document.GetElementById("BirthYear").SetAttribute("value", birthYear);
            
         }
 
@@ -41,6 +46,11 @@ namespace Gmail_MS_builder
         {
            
             webBrowser1.Navigate("https://accounts.google.com/SignUp?service=mail&amp;continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&amp;ltmpl=default");
+        }
+
+        private void btn_MS_Live_Click(object sender, EventArgs e)
+        {
+            webBrowser2.Navigate("https://signup.live.com/?wa=wsignin1.0&rpsnv=13&ct=1488906812&rver=6.7.6636.0&wp=MBI_SSL&wreply=https%3a%2f%2faccount.microsoft.com%2fauth%2fcomplete-signin%3fru%3dhttps%253a%252f%252faccount.microsoft.com%252f%253frefd%253daccount.microsoft.com&id=292666&lw=1&fl=easi2&contextid=F34CA29B7FB8DE6C&bk=1488906814&uiflavor=web&uaid=0c93963462344789b3ae291c44de66e1&mkt=EN-US&lc=1033&lic=1");
         }
     }
 }
